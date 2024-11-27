a6be99758ef24864377ed86f4116258cbd891689
Emir Çoban <emircobanoffical@gmail.com>
2024-11-27 09:48:02 +0300

    Add project files.

---
+++ CSharpEğitimKampi301.DataAccessLayer/Abstract/ICustomerDal.cs
@@ -0,0 +1,14 @@
+﻿using CSharpEğitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEğitimKampi301.DataAccessLayer.Abstract
+{
+    public interface ICustomerDal : IGenericDal<Customer>
+    {
+
+    }
+}
---
+++ CSharpEğitimKampi301.DataAccessLayer/Abstract/IOrderDal.cs
@@ -0,0 +1,13 @@
+﻿using CSharpEğitimKampi301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEğitimKampi301.DataAccessLayer.Abstract
+{
+    public interface IOrderDal : IGenericDal<Order>
+    {
+    }
+}
